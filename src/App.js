12fab9932921a8054bcc9067c920f7ef64a8ce19
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-18 15:41:16 +0200

    My Commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,95 @@
 import React from "react";
-import Box from './components/Box'
-import './style/global.css'
+import Box from "./components/Box";
+import "./style/global.css";
 
-const tempMin = -20
-const tempMax = 40
-const heartMin = 80
-const heartMax = 180
-const stepsMin = 0
-const stepsMax = 50000
-
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 class App extends React.Component {
+  constructor(value) {
+    super();
+    this.state = {
+      heartValue: heartMax,
+      stepsValue: stepsMax,
+      tempValue: tempMax,
+      waterValue: 1.5,
+    };
+  }
+
+  onHeartChange = (event) => {
+    this.setState({ heartValue: event.target.value });
+  };
+  onStepsChange = (event) => {
+    this.setState({ stepsValue: event.target.value });
+  };
+  onTempChange = (event) => {
+    this.setState({ tempValue: event.target.value });
+  };
+  calculateWater = () => {
+    let calculatedWater = 1.5;
+    if (this.state.tempValue > 20) {
+      calculatedWater = calculatedWater + (this.state.tempValue - 20) * 0.02;
+    }
+    if (this.state.heartValue > 120) {
+      calculatedWater = calculatedWater + (this.state.tempValue - 120) * 0.0008;
+    }
+    if (this.state.stepsValue > 10000) {
+      calculatedWater =
+        calculatedWater + (this.state.stepsValue - 10000) * 0.00002;
+    }
+    this.setState({ waterValue: calculatedWater });
+    return this.state.waterValue;
+  };
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <p>Heart : {heartMin}</p>
-          <p>Temperature : {tempMin}</p>
-          <p>Steps : {stepsMin}</p>
-        </div>
-
+      <div className="mt-5 container-fluid">
         <div className="row">
           {/* Water */}
-          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          <Box
+            icon="local_drink"
+            color="#3A85FF"
+            value={this.state.waterValue}
+            unit="L"
+          />
 
           {/* Steps */}
-          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={this.state.stepsValue}
+            unit="steps"
+            min={stepsMin}
+            max={stepsMax}
+            onInput={this.onStepsChange}
+            onChange={this.calculateWater}
+          />
 
           {/* Heart */}
-          <Box icon="favorite" color="red" value={120} unit="bpm" />
+          <Box
+            icon="favorite"
+            color="red"
+            value={this.state.heartValue}
+            unit="bpm"
+            min={heartMin}
+            max={heartMax}
+            onInput={this.onHeartChange}
+            onChange={this.calculateWater}
+          />
 
           {/* Temperature */}
-          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+          <Box
+            icon="wb_sunny"
+            color="yellow"
+            value={this.state.tempValue}
+            unit="°C"
+            min={tempMin}
+            max={tempMax}
+            onInput={this.onTempChange}
+            onChange={this.calculateWater}
+          />
         </div>
       </div>
     );
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+let Div = styled.div`
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, ${parseInt(Math.random() * 255)}, 100) 0%,
+    rgba(9, 9, ${parseInt(Math.random() * 255)}, 1) 35%,
+    rgba(0, ${parseInt(Math.random() * 255)}, 255, 1) 100%
+  );
+  color: white;
+`;
 
 class Box extends React.Component {
   render() {
+    // const Div = this.Div;
     return (
-      <div className="box col-sm-3 col-6">
+      <Div className="box col-sm-3 col-6">
         <span
-          style={{ fontSize: 100, color: this.props.color }} class="material-icons"
+          style={{ fontSize: 100, color: this.props.color }}
+          class="material-icons"
         >
           {this.props.icon}
         </span>
-        <p>{this.props.value} {this.props.unit}</p>
-      </div>
-    )
+        <p style={{ fontSize: "20px" }}>
+          <strong>
+            {this.props.value} {this.props.unit}
+          </strong>
+        </p>
+        {this.props.icon !== "local_drink" && (
+          <input
+            type="range"
+            value={this.props.value}
+            min={this.props.min}
+            max={this.props.max}
+            onInput={this.props.onInput}
+            onChange={this.props.onChange}
+            step={this.props.unit === "steps" && 100}
+          ></input>
+        )}
+      </Div>
+    );
   }
-};
+}
 
 export default Box;
